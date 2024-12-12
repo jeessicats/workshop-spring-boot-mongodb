7d7491be72521e5d2c75012b9a00745d36f2e23d
Jessica <jeessicats@gmail.com>
2024-12-12 14:49:37 -0300

    Adição de Comentários Alinhados aos Posts

--- src/main/java/com/jeessicats/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jeessicats/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.jeessicats.workshopmongo.config;
 import com.jeessicats.workshopmongo.domain.Post;
 import com.jeessicats.workshopmongo.domain.User;
 import com.jeessicats.workshopmongo.dto.AuthorDTO;
+import com.jeessicats.workshopmongo.dto.CommentDTO;
 import com.jeessicats.workshopmongo.repository.PostRepository;
 import com.jeessicats.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,30 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("22/03/2018"), "The burnt toast drama 🍞",
                 "It’s amazing how 5 seconds can turn a perfect breakfast into smoke and despair. R.I.P., my toast.",
                 new AuthorDTO(olivia));
+
+        CommentDTO c1 = new CommentDTO(
+                "I thought it was just me! Maybe the socks are starting a secret society somewhere. 😂",
+                sdf.parse("21/03/2018"),
+                new AuthorDTO(aurora)
+        );
+        CommentDTO c2 = new CommentDTO(
+                "This is why I only buy identical socks. Problem solved... or at least hidden!",
+                sdf.parse("21/03/2018"),
+                new AuthorDTO(olivia)
+        );
+        CommentDTO c3 = new CommentDTO(
+                "The real tragedy is when you try to scrape it off, and it still smells burnt. 😅",
+                sdf.parse("22/03/2018"),
+                new AuthorDTO(john)
+        );
+        CommentDTO c4 = new CommentDTO(
+                "Every toast deserves a second chance... unless it’s completely blackened like mine always ends up.",
+                sdf.parse("22/03/2018"),
+                new AuthorDTO(aurora)
+        );
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3, c4));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
