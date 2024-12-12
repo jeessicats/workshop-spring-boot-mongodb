ddb3330f28a2f6ebe7b67e16f2fb9f51c615fecf
Jessica <jeessicats@gmail.com>
2024-12-12 16:30:26 -0300

    Implementação de Consulta Simples com Query Methods no Spring Data

--- src/main/java/com/jeessicats/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jeessicats/workshopmongo/resources/PostResource.java
@@ -1,10 +1,14 @@
 package com.jeessicats.workshopmongo.resources;
 
 import com.jeessicats.workshopmongo.domain.Post;
+import com.jeessicats.workshopmongo.resources.util.URL;
 import com.jeessicats.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,5 +21,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @RequestMapping(value = "/{id}/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        List<Post> posts = postService.findByTitle(text);
+        return ResponseEntity.ok().body(posts);
     }
 }
