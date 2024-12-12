a557096cc32a09fb8c66789b7d6b86566f4ca25e
Jessica <jeessicats@gmail.com>
2024-12-12 17:40:11 -0300

    Implementação de Consulta com Vários Critérios no Spring Data

--- src/main/java/com/jeessicats/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jeessicats/workshopmongo/resources/PostResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -28,5 +29,16 @@ public class PostResource {
         text = URL.decodeParam(text);
         List<Post> posts = postService.findByTitle(text);
         return ResponseEntity.ok().body(posts);
+    }
+
+    @RequestMapping(value = "/{id}/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+                                                 @RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                                 @RequestParam(value = "maxDate", defaultValue = "") String maxDate) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/jeessicats/workshopmongo/resources/util/URL.java
+++ src/main/java/com/jeessicats/workshopmongo/resources/util/URL.java
@@ -2,10 +2,25 @@ package com.jeessicats.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class URL {
 
     public static String decodeParam(String text) throws UnsupportedEncodingException {
         return URLDecoder.decode(text, "UTF-8");
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
     }
 }
